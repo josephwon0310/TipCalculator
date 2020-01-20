2b09f7a5eeb8d5c9e22cc7eb638b75d531adf326
josephwon0310 <josephwon0310@gmail.com>
2020-01-20 18:05:22 -0500

    Initial Commit

---
+++ TipCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TipCalculator
+//
+//  Created by Joseph Won on 1/20/20.
+//  Copyright © 2020 Joseph Won. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
